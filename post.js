7e3853933042ad5993ba6c3bacfb561f9479b9ff
walolo <61816190+walolo@users.noreply.github.com>
2020-11-04 11:35:11 +0800

    finish the page UI

--- post.js
+++ post.js
@@ -1,3 +1,4 @@
+
 let storedata = function(){
 
 let title = app.get("#title").value
@@ -21,3 +22,44 @@ app.addDate=function(){
 //-------------------------------------< The New Part>-----------
 
 //Get friend list and show on query seleciton
+app.getFriendList =async function(){
+    let frienduser=[]
+    await db.collection("user_kevin").doc(app.loginId).collection("friend").get().then((querySnapshot)=>{
+        querySnapshot.forEach((doc)=>{
+            let data =doc.data()
+            let user={
+                id:data.id,
+                name:data.name,
+                email:data.email
+            }
+            frienduser.push(user)
+        })
+    }).then(()=>console.log(frienduser)).catch((err)=>console.log(err))
+    return frienduser
+}
+
+app.appendList=async function(users){
+    users = await users
+    let searchidtag = app.get(".searchbyidtag")
+    let searchid = app.get(".searchbyid")
+    users.forEach((user)=>{
+        let frienda = document.createElement("option");
+        let friendb = document.createElement("option");
+        frienda.textContent= user.id;
+        friendb.textContent= user.id;
+        searchidtag.appendChild(frienda);
+        searchid.appendChild(friendb)
+    })
+}
+
+
+
+
+
+
+app.init=function(){
+    app.getLoginUser()
+    app.appendList(app.getFriendList())
+}
+
+app.init()
